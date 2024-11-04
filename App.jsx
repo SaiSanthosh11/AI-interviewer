5dd7eec94ca59d2e1d28b6a9b5c71463281ba584
SaiSanthosh11 <saisanthosh3851@gmail.com>
2024-11-04 12:57:41 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import SignupPage from './components/SignupPage'
+
+function App() {
+  
+
+  return (
+    <div>
+      <SignupPage />
+    </div>
+  )
+}
+
+export default App
